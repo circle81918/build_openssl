0f141f5de6b550a136c62c553b051221433f0b69
Rainie <circle81918@gmail.com>
2022-12-12 19:14:31 +0800

    refine main.yml

--- build.bat
+++ build.bat
@@ -1,12 +1,13 @@
 REM Set vc config
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" %1
+cd openssl
 
 REM Set perl config
 if "%1"=="x64" perl Configure VC-WIN64A
 if "%1"=="x86" perl Configure VC-WIN32
 
 REM Build
-cd openssl
+
 nmake
 nmake test
 nmake install
