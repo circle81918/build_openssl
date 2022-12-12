9873f439b305326103e59a45976c78af027261ff
Rainie <circle81918@gmail.com>
2022-12-12 19:06:23 +0800

    refine main.yml

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 REM Set vc config
-call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat" %1
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" %1
 
 REM Set perl config
 if "%1"=="x64" perl Configure VC-WIN64A
